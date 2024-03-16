dcb4a9e873cd3f6581e0584d5a0e89a6444e1937
Sean Archer Tesoro <plankton2403@gmail.com>
2024-03-16 10:18:50 +0800

    added files

--- it-sysarch32-78447-activity3/src/pokedex.jsx
+++ it-sysarch32-78447-activity3/src/pokedex.jsx
@@ -1,27 +1,43 @@
+import React, { useState, useEffect } from "react";
 import Pokemon from "./pokemon";
-import './index.css';
-function Pokedex(){
-            const click1 = () => {studentlist.id};
-    const list = {
-      
-    }
-  
-    return(
-      <>
-      <button onClick = {click1}>Test</button>
-        {Object.keys(list).map((key) => {
-          const studentlist = list[key];
-          return (
-            <Student
-              key={key}
-              id={studentlist.id}
-              name={studentlist.name}
-              email={studentlist.email}
-            />
-          );
-          })}
-      </>
-    )
+import "./index.css";
+
+function Pokedex() {
+  const [language, setLanguage] = useState("english");
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://us-central1-it-sysarch32.cloudfunctions.net/pokemon';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setPokemonList(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  const handleClick = (lang) => {
+    setLanguage(lang);
+  };
+
+  return (
+    <>
+    <div>
+      <button onClick={() => handleClick("english")}>English</button>
+      <button onClick={() => handleClick("japanese")}>Japanese</button>
+      <button onClick={() => handleClick("chinese")}>Chinese</button>
+      <button onClick={() => handleClick("french")}>French</button>
+      </div>
+      {pokemonList.map((pokemon) => (
+        <Pokemon
+          key={pokemon.id}
+          id={pokemon.id}
+          type={pokemon.type}
+          base={pokemon.base}
+          name={pokemon.name[language]}
+          image={pokemon.image}
+        />
+      ))}
+    </>
+  );
 }
 
-export default Pokedex
+export default Pokedex;
--- it-sysarch32-78447-activity3/src/pokemon.jsx
+++ it-sysarch32-78447-activity3/src/pokemon.jsx
@@ -1,12 +1,24 @@
-import './index.css';
-function Pokemon({ name, email,id,}){
-   return(
-    <div className="pokedex">
-         <h1>{id}</h1>
-         <p>{name}</p>
-         <p>{email}</p>
+
+import "./index.css";
+
+function Pokemon({ name, type, base, image, id }) {
+  const baseStats = Object.entries(base);
+
+  return (
+    <div className="Pokemon">
+      <img src={image} alt={name} />
+      <p>{id}</p>
+      <p>{name}</p>
+      <p>{type}</p>
+      <ul>
+        {baseStats.map(([stat, value]) => (
+          <li key={stat}>
+            {stat}: {value}
+          </li>
+        ))}
+      </ul>
     </div>
-   )
+  );
 }
 
-export default Pokemon
+export default Pokemon;
